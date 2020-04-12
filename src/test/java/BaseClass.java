e5bf20dabc77c801a027470997d5964fb9317b41
Dimactik <dimactik@mail.ru>
2020-04-12 13:28:25 -0400

    Login TC

--- src/test/java/BaseClass.java
+++ src/test/java/BaseClass.java
@@ -1,13 +1,15 @@
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.WebDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
 
 
 public class BaseClass {
     WebDriver driver;
 
-
+    @BeforeMethod
     public void browserSetup(){
         ChromeOptions options = new ChromeOptions();
         options.addArguments("ignore-certificate-errors");
@@ -15,6 +17,7 @@ public class BaseClass {
         driver.manage().window().maximize();
     }
 
+    @AfterMethod
     public void browserQuit(){
         driver.quit();
     }
